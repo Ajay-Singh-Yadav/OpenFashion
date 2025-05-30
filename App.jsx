b54d324220aba5803edf65cb47f57a1be9a8b178
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-05-30 13:59:32 +0530

    clean the project

--- App.jsx
+++ App.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
-
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import ProductDrawer from './exploreScreen/DrawerNav/ProductDrawer';
 
 import SplashScreen from './src/Screens/SplashScreen';
 import HomeScreen from './src/Screens/HomeScreen';
@@ -16,34 +12,18 @@ import ProductDetailsScreen from './src/Screens/ProductDetailsScreen';
 import ProductCartScreen from './src/Screens/ProductCartScreen';
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
 
-const MainStack = () => (
-  <Stack.Navigator screenOptions={{headerShown: false}}>
-    <Stack.Screen name="Splash" component={SplashScreen} />
-    <Stack.Screen name="Home" component={HomeScreen} />
-    <Stack.Screen name="ListGrid" component={ProductListGridScreen} />
-    <Stack.Screen name="ProductScreen" component={ProductDetailsScreen} />
-    <Stack.Screen name="CartScreen" component={ProductCartScreen} />
-  </Stack.Navigator>
-);
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Drawer.Navigator
-          drawerContent={props => <ProductDrawer {...props} />}
-          screenOptions={{headerShown: false}}>
-          <Drawer.Screen
-            name="Main"
-            component={MainStack}
-            options={{
-              drawerLabel: () => null,
-              title: null,
-              drawerItemStyle: {height: 0},
-            }}
-          />
-        </Drawer.Navigator>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="ListGrid" component={ProductListGridScreen} />
+          <Stack.Screen name="ProductScreen" component={ProductDetailsScreen} />
+          <Stack.Screen name="CartScreen" component={ProductCartScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
